aa40baeb901623f1be9bb12b5a5c2af1ff47cd39
bartoszpanek1 <panekbartosz1@gmail.com>
2019-12-29 00:07:20 +0100

    Completed Linear Regression class and executed it in main

--- linear_regression/lin_reg.py
+++ linear_regression/lin_reg.py
@@ -1,19 +1,33 @@
 import numpy as np
+
+
 class LinearRegression:
 
-    def __init__(self,data_X,data_y,lambda_reg=0): #regularization parameter lambda is optional (someone may not want to regularize linear regression)
-        self.X=data_X
-        self.y=data_y
-        self.lambda_reg=lambda_reg
-        self.theta=np.zeros((self.X.shape[1]+1,1))
-        self.m=self.X.shape[0] # number of training examples
+    def __init__(self, data_X, data_y,
+                 lambda_reg=0):  # regularization parameter lambda is optional (someone may not want to regularize linear regression)
+        self.m = data_X.shape[0]  # number of training examples
+        self.X = np.append(np.ones((self.m, 1)), data_X, 1)  # training data
+        self.y = data_y  # training answers to the given data
+        self.lambda_reg = lambda_reg  # regularization parameter lambda
+        self.theta = np.zeros((self.X.shape[1], 1))  # initial parameters - all zeros
+        self.cost = self.__compute_cost()  # initial cost based on initial theta
 
-    #using a Mean Squared Error cost function
-    def compute_cost(self):
-        cost = np.square((self.X*self.theta - self.y))
-        if self.lambda_reg>0: # if regularization parameter lambda is greater than 0 we need to apply regularization
+    # using a Mean Squared Error cost function
+    def __compute_cost(self):
+        cost = np.sum(np.square((self.X @ self.theta - self.y))) / (2 * self.m)
+        if self.lambda_reg > 0:  # if regularization parameter lambda is greater than 0 we need to apply regularization
             reg_term = self.lambda_reg * (np.transpose(self.theta) @ self.theta)
             cost = cost + reg_term
         return cost
 
-
+    # using Gradient Descent algorithm to get optimal parameters theta
+    # function returns list of costs (length the same as gradient descent's number of iterations)
+    # iters - number of iterations for GD, needs to be big enough to see a convergence, not useful to be very big because GD will return similar values anyway and will work longer
+    # alpha - learning rate, it can't be too small because GD will be slow to converge and it ca't be too big because GD would fail to converge
+    def compute_optimal_theta(self, iters, alpha):
+        costs = []
+        for i in range(1, iters + 1):
+            self.theta = self.theta * (1 - alpha * self.lambda_reg / self.m) - (alpha / self.m) * (
+                        np.transpose(self.X) @ (self.X @ self.theta - self.y))
+            costs.append(self.__compute_cost())
+        return costs
--- linear_regression/main.py
+++ linear_regression/main.py
@@ -2,21 +2,50 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from linear_regression.lin_reg import LinearRegression
+
 # first we will execute an univariate linear regression
 
-#data1.txt is a data taken from Andrew Ng's Machine Learning course - Linear Regression
-#in data1.txt, first column is the population of a city (in 10000s) and the second column is a profit of a food truck (int $10000s)
+# data1.txt is a data taken from Andrew Ng's Machine Learning course - Linear Regression
+# in data1.txt, first column is the population of a city (in 10000s) and the second column is a profit of a food truck (int $10000s)
 
 
-#showing the data
-data=pd.read_csv('data1.txt',header=None)
-data.columns=['Population','Profit']
+# showing the data
+data = pd.read_csv('data1.txt', header=None)
+data.columns = ['Population', 'Profit']
 print(data.head())
 
 # visualising the data
-data.columns=[0, 1] #changing columns' headers because plt.scatter doesnt see values propely if column's name is a string
-plt.scatter(data[0],data[1])
+data.columns = [0,
+                1]  # changing columns' headers because plt.scatter doesnt see values propely if column's name is a string
+plt.scatter(data[0], data[1])
 plt.xlabel('Population of a City in 10000s')
 plt.ylabel('Profit in $10000s')
+plt.title('Training data')
 plt.show()
 
+X = data[0].to_numpy(copy=True).reshape((data[0].shape[0], 1))
+y = data[1].to_numpy(copy=True).reshape((data[1].shape[0], 1))
+
+lr = LinearRegression(X, y)
+iters = 1500  # no. of iterations in Gradient Descent
+alpha = 0.01  # learning rate
+
+# visualizing the result
+costs = lr.compute_optimal_theta(iters, alpha)
+plt.plot([i for i in range(1, iters + 1)], costs)
+plt.xlabel('Number of iterations')
+plt.ylabel('Cost')
+plt.title('Cost minimization')
+plt.show()  # how gradient minimized the cost
+
+print('Minimized cost: ' + str(lr.cost))
+print('Optimal parameters: ')
+print(lr.theta)
+
+predicted_values = np.append(np.ones((X.shape[0], 1)), X, 1) @ lr.theta
+plt.scatter(data[0], data[1])
+plt.plot(data[0], predicted_values, 'r')
+plt.xlabel('Population of a City in 10000s')
+plt.ylabel('Profit in $10000s')
+plt.title('Optimal line')
+plt.show()
