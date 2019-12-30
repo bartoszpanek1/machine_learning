088ca6008d5116bbf0b513dc5a752ec5b075b2b0
bartoszpanek1 <panekbartosz1@gmail.com>
2019-12-31 00:26:11 +0100

    Added an example of polynomial regression

--- linear_regression/main.py
+++ linear_regression/main.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from linear_regression.lin_reg import LinearRegression
-
+from linear_regression.lin_reg import simple_generate_data
 # first we will execute an univariate linear regression
 
 # data1.txt is a data taken from Andrew Ng's Machine Learning course - Linear Regression
@@ -37,7 +37,10 @@ iters = 1500  # no. of iterations in Gradient Descent
 alpha = 0.01  # learning rate
 
 # visualizing the result
+
+
 costs = lr.compute_optimal_theta(iters, alpha)
+
 plt.plot([i for i in range(1, iters + 1)], costs)
 plt.xlabel('Number of iterations')
 plt.ylabel('Cost')
@@ -97,3 +100,91 @@ print(lr_multi.theta)
 
 print('Predicted price of 1650 sq-ft, 3br house: $' + str(round(lr_multi.predict(np.array([[1650, 3]])), 2)))
 print('Predicted price of 2000 sq-ft, 4br house: $' + str(round(lr_multi.predict(np.array([[2000, 4]])), 2)))
+
+print('PRESS ENTER TO CONTINUE \n')
+print('----------------------')
+print('------POLYNOMIAL------')
+print('----------------------')
+simple_generate_data(3,[10,2,0.3],100)
+data = pd.read_csv('generated_data.txt', header=None)
+
+data.columns = ['X', 'Y']
+data=data.sort_values(by=['X']) #will be useful later to plot the result correctly
+print('First 5 sorted examples')
+print(data.head())
+
+data.columns = [0,
+                1]  # changing columns' headers because plt.scatter doesnt see values propely if column's name is a stringhttps://towardsdatascience.com/polynomial-regression-bbe8b9d97491
+
+plt.scatter(data[0], data[1])
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('Training data - polynomial')
+plt.show()
+
+input('PRESS ENTER TO APPLY LINEAR REGRESSION \n')
+X = data[0].copy(deep=True).to_numpy().reshape((data[0].shape[0], 1))
+y = data[1].copy(deep=True).to_numpy().reshape((data[1].shape[0], 1))
+
+lr = LinearRegression(X, y)
+iters = 1500  # no. of iterations in Gradient Descent
+alpha = 0.01  # learning rate
+
+# visualizing the result
+
+
+costs = lr.compute_optimal_theta(iters, alpha)
+
+plt.plot([i for i in range(1, iters + 1)], costs)
+plt.xlabel('Number of iterations')
+plt.ylabel('Cost')
+plt.title('Cost minimization - univariate')
+plt.show()  # how gradient minimized the cost
+
+print('Minimized cost: ' + str(lr.cost))
+print('Optimal parameters: ')
+print(lr.theta)
+
+predicted_values = lr.X @ lr.theta
+plt.scatter(data[0], data[1])
+plt.plot(data[0], predicted_values, 'r')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('Optimal line')
+plt.show()
+
+
+print('On the graph we can see that simple linear function will not do the job. We need a polynomial')
+print('Lets try to use a second degree polynomial.')
+print('To achieve that, we need to add one more column of X squared')
+input('PRESS ENTER TO APPLY POLYNOMIAL REGRESSION\n')
+X=np.append(X ,np.square(X),1)
+lr = LinearRegression(X, y,normalize=True)
+iters = 1500  # no. of iterations in Gradient Descent
+alpha = 0.01  # learning rate
+
+# visualizing the result
+
+
+costs = lr.compute_optimal_theta(iters, alpha)
+
+plt.plot([i for i in range(1, iters + 1)], costs)
+plt.xlabel('Number of iterations')
+plt.ylabel('Cost')
+plt.title('Cost minimization - univariate')
+plt.show()  # how gradient minimized the cost
+
+print('Minimized cost: ' + str(lr.cost))
+print('Optimal parameters: ')
+print(lr.theta)
+
+predicted_values = lr.X @ lr.theta
+plt.scatter(data[0], data[1])
+plt.plot(data[0], predicted_values, 'r')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('Optimal line')
+plt.show()
+
+print(data[0].shape)
+print(predicted_values.shape)
