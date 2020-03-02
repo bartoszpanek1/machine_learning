4b41890da389bb4085973536b5ee04daa91ca5d4
bartoszpanek1 <panekbartosz1@gmail.com>
2020-03-02 08:02:59 +0100

    changed files names

--- neural_network/main.py
+++ neural_network/main.py
@@ -1,32 +1,65 @@
 from neural_network.n_net import NeuralNetwork
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.io import loadmat
+from random import randrange
+import warnings
 
-a=loadmat('ex3data1.mat',squeeze_me=False)
-
-
-X,y=a["X"], a["y"]
-data=np.asarray(X,dtype=np.float32)
-labels=np.asarray(y,dtype=np.int32)
-
-b=loadmat('ex3weights.mat')
-Theta1,Theta2=b["Theta1"], b["Theta2"]
-
-print(data.shape)
-print(labels.shape)
-print(Theta1.shape)
-print(Theta2.shape)
-print('-----------------------')
-nn=NeuralNetwork(3,[400,25,10],0.1)
-nn.matrices=[Theta1,Theta2]
-p=nn.predict(data)
-
-correct_answ=0
-m=data.shape[0]
-
-for i in range(0,m):
-    if (p[i]+1)==labels[i][0]:
-        correct_answ=correct_answ+1
+warnings.filterwarnings("ignore")
 
 
-print(correct_answ/m)
+a = loadmat('data.mat', squeeze_me=False)
+
+print("------Neural network - Hand Written Digits Recognition------")
+X, y = a["X"], a["y"]
+data = np.asarray(X, dtype=np.float32)
+labels = np.asarray(y, dtype=np.int32) % 10
+m = data.shape[0]
+b = loadmat('weights.mat')
+Theta1, Theta2 = b["Theta1"], b["Theta2"]
+
+#creating neural network object
+nn = NeuralNetwork(3, [400, 25, 10], 0.1)
+
+learning_rate = 0.9
+iterations = 800
+print("Learning on 5000 examples...")
+costs = nn.gradient_descent(data, labels, learning_rate, iterations)
+print("Learning finished")
+plt.plot([i for i in range(1, iterations + 1)], costs)
+plt.xlabel('Number of iterations')
+plt.ylabel('Cost')
+plt.title('Cost minimization - neural network')
+plt.show()  # how gradient minimized the cost
+
+print("Predicting 5000 examples..")
+predictions = nn.predict(X)[0]
+print("Finished predicting")
+correct_counter = 0
+for i in range(0, len(predictions)):
+    if predictions[i] == labels[i]:
+        correct_counter += 1
+
+accuracy = round(correct_counter/m * 100,2)
+print("Training set accuracy: " + str(accuracy) + "%")
+
+input("Press any button...")
+print("---------VISUALIZATION----------")
+
+while True:
+
+    random_choice = randrange(0, m)
+    d = data[random_choice]
+
+    plt.gray()
+    plt.matshow(d.reshape((20,20), order="F"))
+    plt.show()
+
+    predicted = predictions[random_choice]
+    print("Recognized as: "+ str(predicted))
+    choice = input ("Press Enter to continue, write STOP to stop visualization: ")
+    if choice.upper() == "STOP":
+        break
+
+
+
--- neural_network/n_net.py
+++ neural_network/n_net.py
@@ -1,6 +1,8 @@
 import numpy as np
+from collections import deque
 
-#this is my intepretation of neural network used for classification problem
+
+# this is my intepretation of neural network used for classification problem
 # in this intepretation, in case of binary classification (yes or no ), you need to have two output nodes - one for yes and one for no
 # I am aware that binary classification can be done with only one node but I wanted this class to be universal, you can use it with any number of output nodes you want
 class NeuralNetwork:
@@ -9,8 +11,10 @@ class NeuralNetwork:
     # n_units - list of number of units in each layers, length is equal to n_layers
     # e_init - epsilon used to randomly initialize parameters, should be small (eg. 0.1)
     def __init__(self, n_layers, n_units, e_init):
-        if(n_layers!=len(n_units)):
+        if (n_layers != len(n_units)):
             raise Exception('n_layers and len(n_units) must be equal')
+        if (e_init <= 0):
+            raise Exception('e_init must be greater than 0')
         self.n_layers = n_layers
         self.n_units = n_units
         self.e_init = e_init
@@ -24,30 +28,72 @@ class NeuralNetwork:
             self.matrices.append(
                 np.random.rand(self.n_units[i + 1], self.n_units[i] + 1) * 2 * self.e_init - self.e_init)
 
-
-    def cost(self, y_pred,y):
-        if(len(y)!=len(y_pred)):
+    def __cost(self, y_pred, y):
+        if len(y) != len(y_pred):
             raise Exception('y and y_pred must have the same length')
-        num_labels=self.n_units[self.n_layers-1]
-        m=len(y)
-        y_vec=np.zeros((m,num_labels))
-        for i in range(0,num_labels):
-            y_vec[:,i][:,np.newaxis]=np.where(y==i,1,0)
-        cost=(1/m)*np.sum(np.sum(-y_vec*np.log(y_pred) - (1-y_vec)*np.log(1-y_pred)))
+        m = len(y)
+        cost = (1 / m) * np.sum(np.sum(-y * np.log(y_pred) - (1 - y) * np.log(1 - y_pred)))
         return cost
 
-#returns tuple (predicted labels, calculated probabilities)
+    def __labels_to_vector(self, y):
+        num_labels = self.n_units[self.n_layers - 1]
+        m = len(y)
+        y_vec = np.zeros((m, num_labels))
+        for i in range(0, num_labels):
+            y_vec[:, i][:, np.newaxis] = np.where(y == i, 1, 0)
+        return y_vec
+
+    # returns tuple (predicted labels, calculated probabilities)
     def predict(self, X):
         m = X.shape[0]
         units_vals = []
-        a = X
+        a = X.copy()
+        a = np.append(np.ones((m, 1)), a, 1)
         units_vals.append(a)
         for theta in self.matrices:
-            a = np.append(np.ones((m, 1)), a, 1)
             z = a @ np.transpose(theta)
             a = self.__sigmoid(z)
+            a = np.append(np.ones((m, 1)), a, 1)
             units_vals.append(a)
-        return units_vals[len(units_vals) - 1].argmax(axis=1),units_vals[len(units_vals) - 1]
+        units_vals[-1] = np.delete(units_vals[-1], 0, 1)
+
+        return units_vals[len(units_vals) - 1].argmax(axis=1), units_vals
 
     def __sigmoid(self, z):
         return 1 / (1 + np.exp(-z))
+
+    # computing gradient using backpropagation algorithm
+    def gradient_cost(self, X, y):
+        # computing cost
+        # computing gradient using backpropagation
+        m = X.shape[0]
+        y_vec = self.__labels_to_vector(y)
+        pred = self.predict(X)[1]
+        cost = self.__cost(pred[-1], y_vec)
+        theta_grads = [np.zeros(t.shape) for t in self.matrices]
+        y_v = self.__labels_to_vector(y)
+
+        # compute errors
+        sigmas = deque()
+        sigmas.appendleft(pred[-1] - y_v)
+
+        for i in reversed(range(1, len(pred) - 1)):
+            sigmas.appendleft((sigmas[i - 1] @ self.matrices[i][:, 1:]) * pred[i][:, 1:] * (1 - pred[i][:, 1:]))
+
+        # compute gradients
+        for i, t in enumerate(theta_grads):
+            t += np.transpose(sigmas[i]) @ pred[i]
+
+        for i in range(0, len(theta_grads)):
+            theta_grads[i] = theta_grads[i] / m
+
+        return cost, theta_grads
+
+    def gradient_descent(self, X, y, alpha, iters):
+        costs = []
+        for i in range(1, iters + 1):
+            cost, theta_grads = self.gradient_cost(X, y)
+            costs.append(cost)
+            for i in range(0, len(self.matrices)):
+                self.matrices[i] = self.matrices[i] - (alpha * theta_grads[i])
+        return costs
