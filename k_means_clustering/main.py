d7eb6cb2aa7043ad4dd255f7aa46c14e23d80cae
bartoszpanek1 <panekbartosz1@gmail.com>
2020-03-10 08:03:26 +0100

    Added info about the data

--- k_means_clustering/main.py
+++ k_means_clustering/main.py
@@ -2,7 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import loadmat
 from k_means_clustering.k_means import  KMeans
-data = loadmat("ex7data2.mat")
+
+# example data is taken from the Andrew Ng's machine learning course
+
+data = loadmat("data.mat")
 X= data["X"]
 
 k_means = KMeans(X,3,visualize=True)
